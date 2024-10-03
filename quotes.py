f74f09ffe22c45180bea3152e4d832c6b533a4b1
suhelkhanca <suhel.hata@gmail.com>
2024-10-03 23:07:09 +0530

    code for fetching quotes

--- quotes.py
+++ quotes.py
@@ -1,1 +1,15 @@
 # Integrating Random-quotes API into eConnect Project
+
+import requests
+
+url = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+response = requests.get(url)
+
+data  = response.json() # Dictionary
+
+data_body = data["data"]
+quote = data_body["content"]
+author = data_body["author"]
+
+print(quote)
+print(author)
